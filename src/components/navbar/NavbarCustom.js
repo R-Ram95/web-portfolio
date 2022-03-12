52c406df26876c27327f21ba992ef0cfb445fc8e
R-Ram95 <rohinesh@ualberta.ca>
2022-03-11 22:34:16 -0700

    completed about section

--- src/components/navbar/NavbarCustom.js
+++ src/components/navbar/NavbarCustom.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './navbar.scss'
 import Text from '../Text';
 import { Link } from 'react-router-dom';
+import { scroller } from 'react-scroll'
 
 const styles = {
     textStyle: {
@@ -13,22 +14,36 @@ const styles = {
 
 function NavbarCustom() {
     return (
-        <Navbar bg='light' sticky='top' expand="lg">
+        <Navbar bg='light' sticky='top' expand="lg" style={{borderBottom: "2px solid black"}}>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse className='justify-content-center'>
                 <Nav variant="pills" defaultActiveKey="/">
-                    <Nav.Link as={ Link } to="/" eventKey="/" className="me-3" >
+
+                    <Nav.Link 
+                        as={ Link } 
+                        to="/" eventKey="/" 
+                        className="me-3" 
+                        onClick={()=>{scroller.scrollTo('home')}}
+                    >
                         <Text style={styles.textStyle}> Home </Text>
                     </Nav.Link >
-                    <Nav.Link as={ Link } to="/about" className="me-3" eventKey="about">
+
+                    <Nav.Link 
+                        className="me-3" 
+                        eventKey="about" 
+                        onClick={()=> {scroller.scrollTo('about')}}
+                    >
                         <Text style={styles.textStyle}> About </Text>
                     </Nav.Link>
+
                     <Nav.Link as={ Link } to="/projects" className="me-3" eventKey="projects">
                         <Text style={styles.textStyle}> Projects </Text>
                     </Nav.Link>
+
                     <Nav.Link as={ Link } to="/education" className="me-3" eventKey="education">
                         <Text style={styles.textStyle}> Education </Text>
                     </Nav.Link>
+
                     <Nav.Link as={ Link } to="/contact" className="me-3" eventKey="contact">
                         <Text style={styles.textStyle}> Contact </Text>
                     </Nav.Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,75 @@
-import { Container } from 'react-bootstrap'
+import { Container, Image, Stack } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TypeWriter from 'typewriter-effect'
 import Text from '../components/Text';
 
 // css styling
 const styles = {
-    containerStyle: {
+    homeStyle: {
         height: "100vh",
         width: "100%",
         backgroundImage: `url("https://images.pexels.com/photos/10622706/pexels-photo-10622706.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")`,
         backgroundSize: "100% 100%",
         backgroundAttachment: "fixed",
-        backgroundRepeat: "no-repeat"
+        backgroundRepeat: "no-repeat",
+        borderBottom: "4px solid black"
+    },
+    aboutStyle: {
+
+    },
+    imageStyle: {
+        borderStyle: "solid",
+        borderColor: "#3D3D3D",
+        borderWidth: "3px"
     }
 }
 
 function Home() {
     return (
-        <Container fluid className="d-flex flex-column justify-content-center align-items-center" style={styles.containerStyle}>
-            <Text style={{fontSize: "50px", fontWeight: "bold"}}>Hello, my name is Roh.</Text>
-            <Text style={{fontSize: "30px"}}>
-                <TypeWriter
-                    onInit={(typewriter) => {
-                        typewriter
-                            .typeString("I am a Software Engineer and Developer.")
-                            .deleteAll()
-                            .typeString("I am a Mechanical Engineer.")
-                            .deleteAll()
-                            .typeString("Welcome to my site!")
-                            .start()
-                    }}
-                    options={{ loop: true }}
-                />
-            </Text>
-        </Container>
+        <>
+            <Container fluid 
+                id='home' 
+                className="d-flex flex-column justify-content-center align-items-center" 
+                style={styles.homeStyle}
+            >
+                <Text style={{ fontSize: "50px", fontWeight: "bold" }}>Hello, my name is Roh.</Text>
+                <Text style={{ fontSize: "30px" }}>
+                    <TypeWriter
+                        onInit={(typewriter) => {
+                            typewriter
+                                .typeString("I am a Software Engineer and Developer.")
+                                .deleteAll()
+                                .typeString("I am a Mechanical Engineer.")
+                                .deleteAll()
+                                .typeString("Welcome to my site!")
+                                .start()
+                        }}
+                        options={{ loop: true }}
+                    />
+                </Text>
+            </Container>
+
+            <Container className="mt-5 mb-5" id='about'>
+                <Stack direction='horizontal' gap={5}>
+                    <Image 
+                        className='rounded-circle' 
+                        src={process.env.PUBLIC_URL + '/profilepicnb1.png'} 
+                        style={styles.imageStyle} 
+                    />
+                    <div>
+                        <Text style={{ fontSize: '40px', fontWeight: 'bold' }}>About Me</Text>
+                        <Text style={{ fontSize: '20px' }}>
+                            I am a <strong> hard working</strong> and <strong> passionate </strong> developer set to graduate from an M.Eng in Software Engineer
+                            at the University of Calgary in May 2022. I did my undergrad in Mechanical Engineering
+                            at the University of Alberta where I graduated with distinction. I love all things
+                            tech related and frequently participate in hackathons and pursue extracurricular projects. My interests include
+                            full stack development, devops, and machine learning. Thank you so much for browsing my website, and please make sure
+                            to checkout my projects!
+                        </Text>
+                    </div>
+                </Stack>
+            </Container>
+        </>
     )
 }
 
