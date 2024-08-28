4d00ab8f78bef1c37ae3d9f156008babb56443f6
R-Ram95 <rohinesh@ualberta.ca>
2024-08-27 18:38:36 -0600

    add navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import RightPanel from "./components/right-panel";
+import LeftPanel from "./components/left-panel";
+
 function App() {
-  return <div className="text-slate-200">My Portfolio Coming Soon!</div>;
+  return (
+    <div className="mx-auto max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-20 lg:py-0">
+      <div className="grid grid-cols-1 lg:grid-cols-2 min-h-screen">
+        <LeftPanel />
+        <RightPanel />
+      </div>
+    </div>
+  );
 }
 
 export default App;
