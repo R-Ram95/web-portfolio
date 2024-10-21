ac7ea98b5d9d48947d8f793196eaa290de5122d4
R-Ram95 <rohinesh@ualberta.ca>
2024-10-21 13:30:12 -0600

    update resume and experience tense

--- src/sections/experience/Experience.data.ts
+++ src/sections/experience/Experience.data.ts
@@ -11,7 +11,7 @@ export const experienceData = [
     position: "Fullstack Engineer - Accolite Digital",
     year: "2022 - 2024",
     description:
-      "Build, style, and ship high-quality web applications for various clients. Notable achievements include refactoring code to reduce graph-building code by 82% while simultaneously enhancing user interactivity and optimizing web application performance with modern client-side caching techniques, reducing load times by 5 seconds. Additionally, collaborated with developers and product managers/owners to drive continuous improvements in user experience.",
+      "Built, styled, and shipped high-quality web applications for various clients. Notable achievements include refactoring code to reduce graph-building code by 32% while simultaneously enhancing user interactivity and optimizing web application performance with modern client-side caching techniques, reducing load times by 5 seconds. Additionally, I collaborated with developers and product managers/owners to drive continuous improvements in user experience.",
     skills: ["React", "Javascript", "NodeJS", "AWS", "Terraform", "Docker"],
     link: "https://www.bounteous.com/",
   },
